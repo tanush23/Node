9e878ed88c0ce5c1a5f5080506cfec1c99f0eb46
tanush23 <113005445+tanush23@users.noreply.github.com>
2022-09-29 22:57:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
+// JavaScript source code
 const http = require("http");
 const fs = require("fs");
-
-var port = require("minimist")(process.argv.slice(2),{
-    default:{
-        greeting:"Hello",
-    },
-});
-
+var port = process.argv.slice(2)
+port = parseInt(String(port).slice(7))
+console.log(port);
+//fs.readFile("home.html", (err, home) => {
+//    console.log(home.toString());
+//});
+//fs.readFile("registration.html", (err, registration) => {
+//    console.log(registration.toString());
+//});
+//fs.readFile("home.html", (err, home) => {
+//    if (err) {
+//        throw err;
+//    }
+//    http
+//        .createServer((request, response) => {
+//            response.writeHeader(200, { "Content-Type": "text/html" });
+//            response.write(home);
+//            response.end();
+//        })
+//        .listen(3000);
+//});
 let homeContent = "";
 let projectContent = "";
-let registrarionContent = "";
-// fs.readFile("home.html",(err, home) => {
-    // console.log(home.toString());
-// });
-fs.readFile("home.html", (err, home)=>{
-    if(err){
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
         throw err;
     }
-    homeContent=home;
-    // http
-    // .createServer((request,response)=>{
-    //     response.writeHeader(200, {"Content-Type":"text/html" });
-    //     response.write(home);
-    //     response.end();
-    // })
-    // .listen(3000);
+    homeContent = home;
 });
-fs.readFile("project.html", (err,project)=>{
-    if(err) {
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
         throw err;
     }
     projectContent = project;
 });
-fs.readFile("registration.html", (err,registration)=>{
-    if(err) {
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
         throw err;
     }
-    registrarionContent=registration;
-})
-http.createServer((request,response)=>{
+    registrationContent = registration;
+});
+http
+    .createServer((request, response) => {
         let url = request.url;
-        response.writeHeader(200, { "Content-Type":"text/html" });
-        switch(url){
-            case "/registration":
-              response.write(registrarionContent);
-              response.end();
-              break;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
             case "/project":
-              response.write(projectContent);
-             response.end();
-              break;
+                response.write(projectContent);
+                response.end();
+                break;
+            case "/registration":
+                response.write(registrationContent);
+                response.end();
+                break;
             default:
-            response.write(homeContent);
-            response.end();
-            break;
+                response.write(homeContent);
+                response.end();
+                break;
         }
-    }).listen(port);
+    })
+    .listen(port);
